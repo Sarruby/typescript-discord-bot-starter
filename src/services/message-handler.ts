5f4bee1b1e6da4f224ae06fa36d16b6054d8f025
M <emilyfranklin.email@gmail.com>
2020-03-22 05:49:57 -0400

    Add checks for valid console use.
    Try `npm run test` to see all functionality added for handling messages.
    In short, we now check the user, the channel, and content for basic
    misuse.
    
    TESTED=ran npm run test, then used the output list to manually test.

--- src/services/message-handler.ts
+++ src/services/message-handler.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import {Message, Client} from 'discord.js';
+import {Message, Client, TextChannel} from 'discord.js';
 // eslint-disable-next-line no-unused-vars
 import {PingCommand} from './message-commands/ping';
 import {inject, injectable} from 'inversify';
@@ -27,13 +27,63 @@ export class MessageHandler {
   /**
     * Handles new messages.
     * @param {Message} message - The message to handle.
-    * @return { boolean} - Is it valid?
+    * @return { { isOk:boolean, promise:Promise<Message | Message[]> } } -
+    * When isOk is true, continue. If !isOk, the promise represents the action,
+    * if any, that was taken in response to the message. The promise is
+    * rejected when there is an error where there is something seriously wrong.
+    * Otherwise, the promise is possibly the promise of a reply or an empty
+    * array (since no action was taken);
     */
-  isValidMessage(message: Message): boolean {
+  validityCheck(message: Message):
+    { isOk:boolean, promise:Promise<Message | Message[]>} {
+    // Ignore other bots + self.
     if (message.author.bot == true) {
-      return false;
+      return {isOk: false, promise: Promise.resolve([])};
     }
-    return true;
+    // Some checks below rely on user.
+    if (this.client.user == null) {
+      return {isOk: false, promise: Promise.reject(
+          new Error(
+              'ERROR: somehow the client.user is null!' +
+          ' Consider restarting.'))};
+    }
+
+    // Only consider text channels.
+    // If Direct Messaged, tell user we can't do anything.
+    if (message.channel.type != 'text') {
+      if (message.channel.type == 'dm') {
+        return {isOk: false, promise: message.reply(
+            'Sorry, I am unequipped to handle direct messages.',
+        )};
+      }
+      // Docs say type will be text or dm but just in case we have this.
+      // Not unit tested.
+      return {isOk: false, promise: Promise.resolve([])};
+    }
+
+    // Only reply if the channel is exactly "console".
+    // If users try to message the bot with @bot, tell them the bot is limited.
+    const textChannel:TextChannel = message.channel;
+    if (textChannel.name != 'console') {
+      if (message.mentions.users.has('' + this.client.user.id) == true) {
+        return {isOk: false, promise: message.reply(
+            'Sorry, I have very limited use for admins only, and will only ' +
+            'respond with this message when mentioned.',
+        )};
+      }
+      return {isOk: false, promise: Promise.resolve([])};
+    }
+
+    // When the messsage is in the console, don't let mentions of
+    // the bot interfere with
+    // the raw text input.
+    if (message.mentions.users.has('' + this.client.user.id) == true) {
+      return {isOk: false, promise: message.reply(
+          'When using the console, do not @bot - just type the commands.',
+      )};
+    }
+
+    return {isOk: true, /* ignore */promise: Promise.resolve([])};
   }
 
   /**
@@ -42,14 +92,16 @@ export class MessageHandler {
     * @return { Promise<Message | Message[]>} - Whatever the handler gives back.
     */
   handle(message: Message): Promise<Message | Message[]> {
-    if (!this.isValidMessage(message)) {
-      return Promise.reject(message);
+    const validityCheck:{ isOk:boolean, promise:Promise<Message | Message[]>} =
+     this.validityCheck(message);
+    if (!validityCheck.isOk) {
+      return validityCheck.promise;
     }
 
     if (this.pingCommand.isCommandCalled(message)) {
       return this.pingCommand.doCommand(message);
     }
 
-    return Promise.reject(message);
+    return message.reply('Unknown command! TODO(M) Make list.');
   }
 }
--- tests/services/message-handler.spec.ts
+++ tests/services/message-handler.spec.ts
@@ -3,37 +3,72 @@ import 'mocha';
 import {expect} from 'chai';
 import {PingCommand} from "../../src/services/message-commands/ping";
 import {MessageHandler} from "../../src/services/message-handler";
-import {instance, mock, verify, when} from "ts-mockito";
-import {Message, Client, User} from "discord.js";
+import {instance, mock, verify, when, anything, anyString} from "ts-mockito";
+import {Message, Client, ClientUser, TextChannel, DMChannel, MessageMentions, User, Collection} from "discord.js";
 
 describe('MessageHandler', () => {
   let mockedClientClass: Client;
   let mockedClientInstance: Client;
+  let mockedClientUserClass: ClientUser;
+  let mockedClientUserInstance: ClientUser;
+
   let mockedPingCommandClass: PingCommand;
   let mockedPingCommandInstance: PingCommand;
+
   let mockedMessageClass: Message;
   let mockedMessageInstance: Message;
-  let mockedAuthorUserClass: User;
-  let mockedAuthorUserInstance: User;
+  let kOriginalMessage = 'original message';
+  let mockedAuthorUserClass: ClientUser;
+  let mockedAuthorUserInstance: ClientUser;
+  let mockedTextChannelClass: TextChannel;
+  let mockedTextChannelInstance: TextChannel;
+  let mockedDMChannelClass: DMChannel;
+  let mockedDMChannelInstance: DMChannel;
+  let mockedMessageMentionsClass: MessageMentions;
+  let mockedMessageMentionsInstance: MessageMentions;
+  let mentionedUsers:Collection<string, User> = new Collection<string, User>();
 
   let service: MessageHandler;
 
   beforeEach(() => {
     mockedClientClass = mock(Client);
     mockedClientInstance = instance(mockedClientClass);
+    mockedClientUserClass = mock(ClientUser);
+    mockedClientUserInstance = instance(mockedClientUserClass);
+
     mockedPingCommandClass = mock(PingCommand);
     mockedPingCommandInstance = instance(mockedPingCommandClass);
+
     mockedMessageClass = mock(Message);
     mockedMessageInstance = instance(mockedMessageClass);
-    mockedAuthorUserClass = mock(User);
+    mockedAuthorUserClass = mock(ClientUser);
     mockedAuthorUserInstance = instance(mockedAuthorUserClass);
     mockedMessageInstance.author = mockedAuthorUserInstance;
+    mockedTextChannelClass = mock(TextChannel);
+    mockedTextChannelInstance = instance(mockedTextChannelClass);
+    mockedTextChannelInstance.type = 'text';
+    mockedDMChannelClass = mock(DMChannel);
+    mockedDMChannelInstance = instance(mockedDMChannelClass);
+    mockedDMChannelInstance.type = 'dm';
+    mockedMessageMentionsClass = mock(MessageMentions);
+    mockedMessageMentionsInstance = instance(mockedMessageMentionsClass);
 
     service = new MessageHandler(mockedClientInstance, mockedPingCommandInstance);
   })
 
-  it('should reply', async () => {
+  function makeValidMessage() {
+    mockedMessageInstance.content = kOriginalMessage;
+    mockedAuthorUserInstance.bot = false;
+    mockedClientInstance.user = mockedClientUserInstance; // can't be null
+    mockedMessageInstance.channel = mockedTextChannelInstance;
+    mockedTextChannelInstance.name = 'console';
+    mockedMessageInstance.mentions = mockedMessageMentionsInstance;
+    mockedMessageMentionsInstance.users = mentionedUsers;
+  }
+
+  it('for valid message: calls doCommand', async () => {
     makeValidMessage();
+
     when(mockedPingCommandClass.isCommandCalled(mockedMessageInstance))
       .thenReturn(true);
 
@@ -42,37 +77,83 @@ describe('MessageHandler', () => {
     verify(mockedPingCommandClass.doCommand(mockedMessageInstance)).once();
   })
 
-  it('should not reply to bot', async () => {
-    mockedAuthorUserInstance.bot = true;
-    mockedMessageClass.author = mockedAuthorUserInstance;
-    when(mockedPingCommandClass.isCommandCalled(mockedMessageInstance))
-      .thenReturn(true);
+  it('for message by bot: ignores', async () => {
+    makeValidMessage();
+    mockedAuthorUserInstance.bot = false;
 
-    await service.handle(mockedMessageInstance).then(() => {
-      // Successful promise is unexpected, so we fail the test
-      expect.fail('Unexpected promise');
-    }).catch(() => {
-    // Rejected promise is expected, so nothing happens here
-    });
+    await service.handle(mockedMessageInstance);
 
-    verify(mockedPingCommandClass.doCommand(mockedMessageInstance)).never();
+    verify(mockedMessageClass.reply()).never();
   })
 
-  it('should not activate command when isCommandCalled is false', async () => {
+  it('for broken client: error', async () => {
+    makeValidMessage();
+    mockedClientInstance.user = null;
+
+    await service.handle(mockedMessageInstance).then((_message:Message | Message[]) => {
+      expect.fail(); // Expecting a failure!
+    }).catch((error:any) => {
+      expect(error).is.not.null;
+      expect(error instanceof Error).is.true;
+    });
+
+    verify(mockedMessageClass.reply()).never();
+  })
+
+  it('for dm message: replies with error message', async () => {
+    makeValidMessage();
+    mockedMessageInstance.channel = mockedDMChannelInstance;
+
+    await service.handle(mockedMessageInstance);
+
+    // TODO(M): Consider making strictEquals or figuring out how to match regex.
+    verify(mockedMessageClass.reply(anyString())).once();
+  })
+
+  it('for message mentioning @bot not in console: replies with error message', async () => {
+    makeValidMessage();
+    mockedTextChannelInstance.name = 'not-console';
+    mockedClientUserInstance.id = 'testClientUserId';
+    let mentionedUsersIncludesBot = new Collection<string, User>();
+    mentionedUsersIncludesBot.set('testClientUserId', mockedClientUserInstance);
+    mockedMessageMentionsInstance.users = mentionedUsersIncludesBot;
+
+    await service.handle(mockedMessageInstance);
+
+    // TODO(M): Consider making strictEquals or figuring out how to match regex.
+    verify(mockedMessageClass.reply(anyString())).once();
+  })
+
+  it('for message not in console: ignores', async () => {
+    makeValidMessage();
+    mockedTextChannelInstance.name = 'not-console';
+
+    await service.handle(mockedMessageInstance);
+
+    verify(mockedPingCommandClass.isCommandCalled(anything())).never();
+})
+
+  it('for message mentioning @bot in console: replies with error message', async () => {
+    makeValidMessage();
+    mockedClientUserInstance.id = 'testClientUserId';
+    let mentionedUsersIncludesBot = new Collection<string, User>();
+    mentionedUsersIncludesBot.set('testClientUserId', mockedClientUserInstance);
+    mockedMessageMentionsInstance.users = mentionedUsersIncludesBot;
+
+    await service.handle(mockedMessageInstance);
+
+    // TODO(M): Consider making strictEquals or figuring out how to match regex.
+    verify(mockedMessageClass.reply(anyString())).once();
+  })
+
+  it('for message with no command otherwise valid: replies with error message', async () => {
+    makeValidMessage();
     when(mockedPingCommandClass.isCommandCalled(mockedMessageInstance))
       .thenReturn(false);
 
-    await service.handle(mockedMessageInstance).then(() => {
-      // Successful promise is unexpected, so we fail the test
-      expect.fail('Unexpected promise');
-    }).catch(() => {
-	 // Rejected promise is expected, so nothing happens here
-    });
+    await service.handle(mockedMessageInstance);
 
-    verify(mockedPingCommandClass.doCommand(mockedMessageInstance)).never();
+    // TODO(M): Consider making strictEquals or figuring out how to match regex.
+    verify(mockedMessageClass.reply(anyString())).once();
   })
-
-  function makeValidMessage() {
-    mockedAuthorUserInstance.bot = false;
-  }
 });
